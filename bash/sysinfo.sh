df81ef1de2c79ac5411039d515ffcb51e312408c
vijohndavidk <108038700+vijohndavidk@users.noreply.github.com>
2022-10-05 11:09:34 -0400

    Update sysinfo.sh

--- bash/sysinfo.sh
+++ bash/sysinfo.sh
@@ -3,9 +3,9 @@
 # This script displays the system information
 
 
-hostname
+hostname -f
 hostnamectl
-hostname
+hostname -I
 echo -e "-------------------------------Disk Usage >80%-------------------------------"
 df -Ph | sed s/%//g | awk '{ if($5 > 80) print $0;}'
 echo ""
