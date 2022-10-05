1a86164a82bb297ce0e78326285fc090f20b8340
vijohndavidk <108038700+vijohndavidk@users.noreply.github.com>
2022-10-05 19:04:24 -0400

    Update sysinfo.sh

--- bash/sysinfo.sh
+++ bash/sysinfo.sh
@@ -15,10 +15,8 @@ echo -e "Hostname:\t\t"`hostname`
 echo FQDN: 
 hostname -f
 echo -e "uptime:\t\t\t"`uptime | awk '{print $3,$4}' | sed 's/,//'`
-echo -e "Manufacturer:\t\t"`cat /sys/class/dmi/id/chassis_vendor`
 echo -e "Product Name:\t\t"`cat /sys/class/dmi/id/product_name`
 echo -e "Version:\t\t"`cat /sys/class/dmi/id/product_version`
-echo -e "Serial Number:\t\t"`cat /sys/class/dmi/id/product_serial`
 echo -e "Machine Type:\t\t"`vserver=$(lscpu | grep Hypervisor | wc -l); if [ $vserver -gt 0 ]; then echo "VM"; else echo "Physical"; fi`
 echo -e "Operating System:\t"`hostnamectl | grep "Operating System" | cut -d ' ' -f5-`
 echo -e "Kernel:\t\t\t"`uname -r`
@@ -30,11 +28,6 @@ echo IP Address :
 ip route
 echo Root file system free space :
 df -hT /home
-echo ""
-echo -e "-------------------------------CPU/Memory Usage------------------------------"
-echo -e "Memory Usage:\t"`free | awk '/Mem/{printf("%.2f%"), $3/$2*100}'`
-echo -e "Swap Usage:\t"`free | awk '/Swap/{printf("%.2f%"), $3/$2*100}'`
-echo -e "CPU Usage:\t"`cat /proc/stat | awk '/cpu/{printf("%.2f%\n"), ($2+$4)*100/($2+$4+$5)}' |  awk '{print $0}' | head -1`
 echo ""
 echo -e "-------------------------------Disk Usage >80%-------------------------------"
 df -Ph | sed s/%//g | awk '{ if($5 > 80) print $0;}'
