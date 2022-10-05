105c6a6e8578f4a9af5b4ef5c63c265cbdf5451b
vijohndavidk <108038700+vijohndavidk@users.noreply.github.com>
2022-10-05 19:23:38 -0400

    Update sysinfo.sh

--- bash/sysinfo.sh
+++ bash/sysinfo.sh
@@ -16,17 +16,14 @@ echo FQDN:
 hostname -f
 echo -e "uptime:\t\t\t"`uptime | awk '{print $3,$4}' | sed 's/,//'`
 echo -e "Product Name:\t\t"`cat /sys/class/dmi/id/product_name`
-echo -e "Version:\t\t"`cat /sys/class/dmi/id/product_version`
 echo -e "Machine Type:\t\t"`vserver=$(lscpu | grep Hypervisor | wc -l); if [ $vserver -gt 0 ]; then echo "VM"; else echo "Physical"; fi`
-echo -e "Operating System:\t"`hostnamectl | grep "Operating System" | cut -d ' ' -f5-`
+echo -e "Operating System name and version:\t"`hostnamectl | grep "Operating System" | cut -d ' ' -f5-`
 echo -e "Kernel:\t\t\t"`uname -r`
 echo -e "Architecture:\t\t"`arch`
 echo -e "Processor Name:\t\t"`awk -F':' '/^model name/ {print $2}' /proc/cpuinfo | uniq | sed -e 's/^[ \t]*//'`
 echo -e "Active User:\t\t"`w | cut -d ' ' -f1 | grep -v USER | xargs -n1`
-echo -e "System Main IP:\t\t"`hostname -I`
-echo IP Address :
-ip route
-echo Root file system free space :
+echo -e "IP Address:\t\t"`hostname -I`
+echo Root file system :
 df -hT /home
 echo ""
 echo -e "-------------------------------Disk Usage >80%-------------------------------"
