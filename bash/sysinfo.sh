da65b8ebf2d451461839192e224f85cf041f3b6d
vijohndavidk <108038700+vijohndavidk@users.noreply.github.com>
2022-10-18 23:36:22 -0400

    Update sysinfo.sh

--- bash/sysinfo.sh
+++ bash/sysinfo.sh
@@ -2,28 +2,33 @@
 # My script - sysinfo.sh
 # This script displays the system information
 
+#hostname of the device.
+HOSTNAME=$(hostname)
 
 
-echo "Report for myvm"
-echo -e "===================="
-echo -e "-------------------------------System Information----------------------------"
-echo -e "Hostname:\t\t"`hostname`
-echo FQDN: 
-hostname -f
-echo -e "uptime:\t\t\t"`uptime | awk '{print $3,$4}' | sed 's/,//'`
-echo -e "Product Name:\t\t"`cat /sys/class/dmi/id/product_name`
-echo -e "Machine Type:\t\t"`vserver=$(lscpu | grep Hypervisor | wc -l); if [ $vserver -gt 0 ]; then echo "VM"; else echo "Physical"; fi`
-echo -e "Operating System name and version:\t"`hostnamectl | grep "Operating System" | cut -d ' ' -f5-`
-echo -e "Kernel:\t\t\t"`uname -r`
-echo -e "Architecture:\t\t"`arch`
-echo -e "Processor Name:\t\t"`awk -F':' '/^model name/ {print $2}' /proc/cpuinfo | uniq | sed -e 's/^[ \t]*//'`
-echo -e "Active User:\t\t"`w | cut -d ' ' -f1 | grep -v USER | xargs -n1`
-echo -e "IP Address:\t\t"`hostname -I`
-echo Root file system :
-df -hT /home
-echo ""
-echo -e "-------------------------------Disk Usage >80%-------------------------------"
-df -Ph | sed s/%//g | awk '{ if($5 > 80) print $0;}'
-echo ""
-echo -e "===================="
+#Setting the systems Fully Qualified Domain Name(FQDN).
+FQDN=$(hostname -f)
 
+
+#Setting the operating system name and version.
+OS_NAME=$(lsb_release -d -s)
+
+
+#Setting the IP address of machine.
+IP_ADDRESS=$(ip a s ens33 | grep -w inet | awk '{print $2}')
+
+#Setting the amount of space available in Root file system
+FREE_DISK_SPACE=$(df -h /dev/sda3 | tail -1 | awk '{print $4}')
+
+
+#Calling Variables to display the Output.
+cat <<EOF
+Report for $HOSTNAME
+
+====================================================================== 
+FQDN: $FQDN
+Operating System name and version: $OS_NAME 
+IP Address: $IP_ADDRESS
+Root Filesystem Free Space: $FREE_DISK_SPACE
+====================================================================== 
+EOF
