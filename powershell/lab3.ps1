9ba3d91dd8e38230b4790e2299d0ed572a35ea90
vijohndavidk <108038700+vijohndavidk@users.noreply.github.com>
2022-11-07 14:32:39 -0500

    lab3

---
+++ powershell/lab3.ps1
@@ -0,0 +1,35 @@
+﻿Get-Ciminstance -Class win32_networkadapterconfiguration |
+where { $_.ipenabled -eq "true"}|
+#?{ $_.ipenabled -eq "True"}|
+Format-Table -AutoS Description, Index, IPAddress, IPSubnet,
+@{n="DNSServersearchorder";
+    e={
+    Switch($_.DNSServersearchorder)
+    {
+        $NULL{
+        $variable1="Value is empty...";
+        $variable1
+    }
+   };
+   if($null -ne $_.DNSServerSearchorder)
+     {
+        $_.DNSServerSearchorder
+     }
+    }
+  },
+@{n="DNSdomain";
+    e={
+    Switch($_.DNSdomain)
+    {
+        $NULL
+        {
+            $variable2="Value is empty...";
+            $variable2
+        }
+    };
+    if($null -ne $_.DNSdomain)
+    {
+        $_.DNSdomain
+    }
+      }
+ }
